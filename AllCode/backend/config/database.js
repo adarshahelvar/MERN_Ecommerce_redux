848ad68698416056c09a0e997a4f988a0aa73a3e
Adarsha A <adarshaadi1997@gmail.com>
2023-09-06 11:25:01 +0530

    DB connect & scheme product created

--- AllCode/backend/config/database.js
+++ AllCode/backend/config/database.js
@@ -1,3 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('')
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    // useCreateIndex has removed from version-16
+    })
+    .then((data) => {
+      console.log(`Db connection to server: ${data.connection.host}`);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+module.exports = connectDatabase;
