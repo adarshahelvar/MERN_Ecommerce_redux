6636fb118167832a7843504d4d6a4c9f6a33b96e
Adarsha A <adarshaadi1997@gmail.com>
2023-09-11 11:18:26 +0530

    Header and Footer

--- AllCode/frontend/src/App.js
+++ AllCode/frontend/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import React, { useEffect } from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import Header from "./component/layout/Header/Header.js";
+import Footer from "./component/layout/Footer/Footer.js";
+import WebFont from "webfontloader";
 
 function App() {
+  // This use effect is running to change font style and import font families
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      HELLO WORLD...!
-    </div>
+    <Router>
+      <Header />
+      <Footer />
+    </Router>
   );
 }
 
