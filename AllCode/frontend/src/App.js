8dfc95992d3ee51af011b7c70872321928f2c22d
Adarsha A <adarshaadi1997@gmail.com>
2023-09-11 12:04:56 +0530

    home section

--- AllCode/frontend/src/App.js
+++ AllCode/frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./component/layout/Header/Header.js";
 import Footer from "./component/layout/Footer/Footer.js";
 import WebFont from "webfontloader";
+import Home from "./component/Home/Home.js";
 
 function App() {
   // This use effect is running to change font style and import font families
@@ -18,6 +19,9 @@ function App() {
   return (
     <Router>
       <Header />
+      <Routes>
+        <Route exact path="/" Component={Home} />
+      </Routes>
       <Footer />
     </Router>
   );
---
+++ AllCode/frontend/src/component/Home/Home.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { CgMouse } from "react-icons/cg";
+import "./Home.css";
+import ProductCard from "./ProductCard.js";
+
+// Temp testing obj
+const product = {
+    name:"Blue Tshirt",
+    images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
+    price: "3000",
+    _id:"abhishek",
+}
+
+const Home = () => {
+  return (
+    <>
+          <div className="banner">
+            <p>Welcome to Ecommerce</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+            </div>
+            <h2 className="homeHeading">Featured Products</h2>
+            <div className="container" id="container">
+            <ProductCard key={product._id} product={product} />
+            <ProductCard key={product._id} product={product} />
+            <ProductCard key={product._id} product={product} />
+            <ProductCard key={product._id} product={product} />
+            <ProductCard key={product._id} product={product} />
+            <ProductCard key={product._id} product={product} />
+            <ProductCard key={product._id} product={product} />
+            <ProductCard key={product._id} product={product} />
+            </div>
+    </>
+  )
+}
+
+export default Home
