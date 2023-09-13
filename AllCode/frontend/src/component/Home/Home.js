be190418b43969bbb823010a7960cff33208ea9e
Adarsha A <adarshaadi1997@gmail.com>
2023-09-13 12:02:40 +0530

    Product detailes

--- AllCode/frontend/src/component/Home/Home.js
+++ AllCode/frontend/src/component/Home/Home.js
@@ -3,7 +3,7 @@ import { CgMouse } from "react-icons/cg";
 import "./Home.css";
 import ProductCard from "./ProductCard.js";
 import MetaData from "../layout/MetaData";
-import { getProduct } from "../../actions/productAction";
+import { clearErrors, getProduct } from "../../actions/productAction";
 import { useSelector, useDispatch } from "react-redux";
 import Loader from '../layout/Loader/Loader';
 import { useAlert } from "react-alert";
@@ -20,11 +20,12 @@ const Home = () => {
   );
 
   useEffect(() => {
-    if(error) {
-      return alert.error(error);
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
     }
     dispatch(getProduct());
-  }, [dispatch, error,alert]);
+  }, [dispatch, error, alert]);
 
   return (
     <>
